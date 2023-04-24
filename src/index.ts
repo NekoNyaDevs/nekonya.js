7f3429506b5346b9dbeb46ead75fb044eac9cf77
TheDogHusky <leredlabo@gmail.com>
2023-04-24 23:27:17 +0200

    Added types

--- src/index.ts
+++ src/index.ts
@@ -3,34 +3,36 @@ import fetch from 'node-fetch';
 const url = "https://nekonya.classy.works/api/";
 let version = "v1";
 
+interface jsonRes {
+    url: string;
+}
 
-export async function get(endpoint: string) {
+export async function get(endpoint: string): Promise<jsonRes> {
     const res = await fetch(url + version + "/" + endpoint);
     return res.json();
 }
 
-
-export async function hug() {
+export async function hug(): Promise<string> {
     const res = await get("random/hug");
     return res.url;
 }
 
-export async function kiss() {
+export async function kiss(): Promise<string> {
     const res = await get("random/kiss");
     return res.url;
 }
 
-export async function pat() {
+export async function pat(): Promise<string> {
     const res = await get("random/pat");
     return res.url;
 }
 
-export async function neko() {
+export async function neko(): Promise<string> {
     const res = await get("random/neko");
     return res.url;
 }
 
-export async function kitsune() {
+export async function kitsune(): Promise<string> {
     const res = await get("random/kitsune");
     return res.url;
 }
