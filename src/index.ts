7f693c94629ffe7c994b35b007e7f03ef61b9a5c
Adam <classy.craft3r@gmail.com>
2023-08-20 18:24:40 +0200

    Added endpoints and new types

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,36 @@
 import fetch from 'node-fetch';
 
 const url = "https://nekonya.classy.works/api/";
-let version = "v1";
+export const version = "v1";
+export const latest = "v1";
 
-interface jsonRes {
+
+type randomJSONRes = {
     url: string;
 }
+type randomEntries = "random/hug" | "random/kiss" | "random/pat" | "random/neko" | "random/kitsune" | "random/slap";
 
+type eightballJSONRes = {
+    answer: string;
+}
+type eightballEntries = "8ball";
+
+type owoifyJSONRes = {
+    result: string;
+}
+type owoifyEntries = `owoify?text=${string}`;
+
+type jsonRes = randomJSONRes | eightballJSONRes | owoifyJSONRes;
+
+
+export async function get(endpoint: randomEntries): Promise<randomJSONRes>;
+export async function get(endpoint: eightballEntries): Promise<eightballJSONRes>;
+export async function get(endpoint: owoifyEntries): Promise<owoifyJSONRes>;
 export async function get(endpoint: string): Promise<jsonRes> {
     const res = await fetch(url + version + "/" + endpoint);
     return res.json();
 }
+
 
 export async function hug(): Promise<string> {
     const res = await get("random/hug");
@@ -35,4 +55,19 @@ export async function neko(): Promise<string> {
 export async function kitsune(): Promise<string> {
     const res = await get("random/kitsune");
     return res.url;
+}
+
+export async function slap(): Promise<string> {
+    const res = await get("random/slap");
+    return res.url;
+}
+
+export async function eightball(): Promise<string> {
+    const res = await get("8ball");
+    return res.answer;
+}
+
+export async function owoify(text: string): Promise<string> {
+    const res = await get(`owoify?text=${text}`);
+    return res.result;
 }
