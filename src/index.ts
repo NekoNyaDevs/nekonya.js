2f870a4347a9987962bf162be35024807a90d617
TheDogHusky <contact@classydev.fr>
2024-09-11 17:30:29 +0200

    Added tests, and fixed the owoify request

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import fetch from 'node-fetch';
+import { RequestInit } from "node-fetch";
 
 const url = "https://nekonya.classydev.fr/api/";
 export const version = "v1";
@@ -17,57 +18,63 @@ type eightballEntries = `8ball?cute=${boolean}` | "8ball";
 type owoifyJSONRes = {
     result: string;
 }
-type owoifyEntries = `owoify?text=${string}`;
+type owoifyEntries = `owoify?text=${string}` | "owoify";
 
 type jsonRes = randomJSONRes | eightballJSONRes | owoifyJSONRes;
 
 
-export async function get(endpoint: randomEntries): Promise<randomJSONRes>;
-export async function get(endpoint: eightballEntries): Promise<eightballJSONRes>;
-export async function get(endpoint: owoifyEntries): Promise<owoifyJSONRes>;
-export async function get(endpoint: string): Promise<jsonRes> {
-    const res = await fetch(url + version + "/" + endpoint);
+export async function req(endpoint: randomEntries, fetchParams?: RequestInit): Promise<randomJSONRes>;
+export async function req(endpoint: eightballEntries, fetchParams?: RequestInit): Promise<eightballJSONRes>;
+export async function req(endpoint: owoifyEntries, fetchParams?: RequestInit): Promise<owoifyJSONRes>;
+export async function req(endpoint: string, fetchParams?: RequestInit): Promise<jsonRes> {
+    const res = await fetch(url + version + "/" + endpoint, fetchParams);
     return res.json();
 }
 
 
 export async function hug(): Promise<string> {
-    const res = await get("random/hug");
+    const res = await req("random/hug");
     return res.url;
 }
 
 export async function kiss(): Promise<string> {
-    const res = await get("random/kiss");
+    const res = await req("random/kiss");
     return res.url;
 }
 
 export async function pat(): Promise<string> {
-    const res = await get("random/pat");
+    const res = await req("random/pat");
     return res.url;
 }
 
 export async function neko(): Promise<string> {
-    const res = await get("random/neko");
+    const res = await req("random/neko");
     return res.url;
 }
 
 export async function kitsune(): Promise<string> {
-    const res = await get("random/kitsune");
+    const res = await req("random/kitsune");
     return res.url;
 }
 
 export async function slap(): Promise<string> {
-    const res = await get("random/slap");
+    const res = await req("random/slap");
     return res.url;
 }
 
 export async function eightball(cute?: boolean): Promise<string> {
     const enableCute = cute || false;
-    const res = await get(`8ball?cute=${enableCute}`);
+    const res = await req(`8ball?cute=${enableCute}`);
     return res.answer;
 }
 
 export async function owoify(text: string): Promise<string> {
-    const res = await get(`owoify?text=${text}`);
+    const res = await req(`owoify`, {
+        method: "POST",
+        body: JSON.stringify({ text }),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
     return res.result;
 }
---
+++ test/index.js
@@ -0,0 +1,5 @@
+const NekoNya = require("../lib");
+
+(async () => {
+    console.log(await NekoNya.owoify("Hello, World!\nI love nekos!"));
+})();
